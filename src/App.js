bb2ab396f782cb78d82181ca9b7a38fc797a3e25
sasikant <nairs999@gmail.com>
2020-07-12 19:22:06 +0530

    Add test video

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
-import {data} from './data';
+import { data } from './data';
 
 function App() {
   return (
     <div className="App">
-      
+      <video controls>
+        <source src={data[0].video.originalUrl} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
     </div>
   );
 }
